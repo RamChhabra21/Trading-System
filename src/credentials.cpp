5b6ddb1e230be492b8f9123d2523b338e972bfa3
Ram Chhabra <mailtoramchhabra@gmail.com>
2025-04-23 12:49:13 +0530

    Update credentials.cpp

--- src/credentials.cpp
+++ src/credentials.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include "credentials.hpp"
 
-std::string client_id = "CN78k_eL";
-std::string client_secret = "2HvU6H62wdQG5znvKd7GjNkcVGIflXwuZX6hxE8WHxs";
+std::string client_id = "";
+std::string client_secret = "";
 std::string base_url = "https://test.deribit.com/api/v2";
 std::string accessToken;
